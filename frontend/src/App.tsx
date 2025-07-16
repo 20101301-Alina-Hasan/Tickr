1600d6a63a2048aaf99dd78cfa4f5c578c7e2edd
Alina <alina.hasan.aman@gmail.com>
2025-07-16 18:43:48 +0600

    feat: setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="hover:cursor-pointer">Click me</Button>
+    </div>
+  )
+}
+
+export default App
