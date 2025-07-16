b6091df0c2f2a8b7aa30c071d07054a5bfd597cb
Alina <alina.hasan.aman@gmail.com>
2025-07-16 19:15:55 +0600

    chore: update login form

--- frontend/src/route/login/components/login.tsx
+++ frontend/src/route/login/components/login.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 
-export const Login: React.FC<React.ComponentProps<"div">> = ({
+export const LoginForm: React.FC<React.ComponentProps<"div">> = ({
     className,
     ...props
 }) => {
